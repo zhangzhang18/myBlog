f82e8a846275d17036d4a988ca56f88aa3c68f3a
zhangchunmeng01 <zhangchunmeng01@daojia.com>
2018-07-18 15:14:30 +0800

    Site updated: 2018-07-18 15:14:28

--- photos/photo.js
+++ photos/photo.js
@@ -19,8 +19,8 @@ photo = {
       imageSize = data[i].split(' ')[0];
       imageX = imageSize.split('.')[0];
       imageY = imageSize.split('.')[1];
-      li += '<div class="card" style="width:330px">' +
-        '<div class="ImageInCard" style="height:' + 330 * imageY / imageX + 'px">' +
+      li += '<div class="card" style="width:500px">' +
+        '<div class="ImageInCard" style="height:' + 500 * imageY / imageX + 'px">' +
         '<a data-fancybox="gallery" href="https://raw.githubusercontent.com/zhangzhang18/photos/master/images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
         '<img src="https://raw.githubusercontent.com/zhangzhang18/photos/master/images/' + imgNameWithPattern + '?raw=true"/>' +
         '</a>' +
