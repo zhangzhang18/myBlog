fd971c9a15a89ac8d7217f777e98aaa4974f9d8a
zhangchunmeng01 <zhangchunmeng01@daojia.com>
2018-07-18 12:48:13 +0800

    Site updated: 2018-07-18 12:48:11

--- photos/photo.js
+++ photos/photo.js
@@ -22,8 +22,8 @@ photo ={
            imageY = imageSize.split('.')[1];
             li += '<div class="card" style="width:330px">' +
                     '<div class="ImageInCard" style="height:'+ 330 * imageY / imageX + 'px">' +
-                      '<a data-fancybox="gallery" href="https://github.com/zhangzhang18/photos/tree/master/images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
-                        '<img src="https://github.com/zhangzhang18/photos/tree/master/images/' + imgNameWithPattern + '?raw=true"/>' +
+                      '<a data-fancybox="gallery" href="https://raw.githubusercontent.com/zhangzhang18/photos/master/images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
+                        '<img src="https://raw.githubusercontent.com/zhangzhang18/photos/master/images/' + imgNameWithPattern + '?raw=true"/>' +
                       '</a>' +
                     '</div>' +
                     // '<div class="TextInCard">' + imgName + '</div>' +
